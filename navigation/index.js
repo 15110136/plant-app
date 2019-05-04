b3d5506de7ce6d20b9c521ec91e2015eb84b828f
15110136 <nhatthong34@gmail.com>
2019-05-04 11:23:43 +0700

    update map

--- navigation/index.js
+++ navigation/index.js
@@ -20,12 +20,12 @@ import { theme } from '../constants';
 const role = 'client'
 
 const client = createStackNavigator({
-  Map,
   Welcome,
   Login,
   SignUp,
   Forgot,
   BookService,
+  Map,
   Profile,
   Explore,
   Product,
